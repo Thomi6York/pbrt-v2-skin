a8c5d17d98ad2d6ccb1fedf1eb4c12d447b6431e
Thomi6York <tw1700@york.ac.uk>
2023-11-01 15:33:50 +0000

    relative file paths

--- BatchProcessAndRender/preprocess_and_save.bat
+++ BatchProcessAndRender/preprocess_and_save.bat
@@ -2,11 +2,12 @@
 setlocal enabledelayedexpansion
 
 rem set our folder locations
-set "OutRep=C:\\Users\\tw1700\\OneDrive - University of York\\Documents\\PhDCore\\Practical\\DJModel\\pbrt-v2-skin\\scenes\\geometry\\ProcDatasetHeads\\"
-set "InRep=C:\\Users\\tw1700\\OneDrive - University of York\\Documents\\PhDCore\\Experiments\\PilotDataSet\\"
+set "OutRep=..\\scenes\geometry\\processed\\"
+set "InRep=..\\scenes\\PilotDataSet\\"
 
 rem the .cpp for obj2pbrt is in src\tools for post 2015 vers of pbrt2. 
 rem ^^ i would build with a g++ compiler in vs code and mingw32 for convenience 
+rem create a conda environment, and simply use conda install pip and pip install pymeshlab to create the env
 
 rem create symbolic links to get around local search Or just use copy solution here if you don't want admin privileges 
 
@@ -16,7 +17,6 @@ python getFolderNames.py "!InRep!" "!OutRep!"
 
 set "file_name=folder_names.txt"
 set "file_path=!OutRep!!file_name!"
-rem echo File path is !file_path!
 
 rem pull into loop  -- use backq delims handles spaces 
 for /f "usebackq delims=" %%i in ("!file_path!") do (
@@ -29,7 +29,7 @@ for /f "usebackq delims=" %%i in ("!file_path!") do (
     python argpreprocess.py "!InRep!!directory_path!/mesh.obj" "!OutRep!!directory_path!meshProc.obj" 
     
     rem make header files available to local folder for processing by copying (make symoblic link if you want)
-    copy "!OutRep!!directory_path!meshProc.obj.mtl" "".\""
+    copy "!OutRep!!directory_path!meshProc.obj.mtl" ".\\"
 
     rem now use the coverter tool to create a pbrt
     obj2pbrt.exe "!OutRep!!directory_path!meshProc.obj" "!OutRep!!directory_path!meshProc.pbrt"
