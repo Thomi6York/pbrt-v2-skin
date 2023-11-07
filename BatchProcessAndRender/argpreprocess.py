c58fddbfb7130cfbbe4317ea65ee4f8e7291259c
Thomi6York <tw1700@york.ac.uk>
2023-11-07 20:09:59 +0000

    Scene and experiments

--- BatchProcessAndRender/argpreprocess.py
+++ BatchProcessAndRender/argpreprocess.py
@@ -10,20 +10,20 @@ def process_mesh(input_file, output_file):
     
 
     # Apply your desired mesh processing operations here
-    ms.compute_matrix_from_scaling_or_normalization(
-        axisx=1.0,
+    ms.compute_matrix_from_scaling_or_normalization( #should scale and normalize as gui, but ignores normals for some reason 
+        axisx=1.0, #axis limits 
         axisy=1.0,
         axisz=1.0,
         uniformflag=True,
-        scalecenter='origin',
-        customcenter=[0.0, 0.0, 0.0],
-        unitflag=True,
-        freeze=True,
-        alllayers=True
+        scalecenter='origin', #scale to origin of space
+        customcenter=[0.0, 0.0, 0.0], #custom centre if you want one
+        unitflag=True, #bounding box
+        freeze=True, # freeze matrix values 
+        alllayers=True #apply to all visible layers
     )
 
-    ms.apply_normal_normalization_per_vertex()
-    ms.compute_texcoord_transfer_wedge_to_vertex()
+    ms.apply_normal_normalization_per_vertex() #normalize normals
+    ms.compute_texcoord_transfer_wedge_to_vertex() #covert wedge coord to tex coords [may not be the case for Will]
     
     # Save the processed mesh
     ms.save_current_mesh(output_file,
@@ -35,7 +35,7 @@ def process_mesh(input_file, output_file):
     
     print("Mesh saved to " + output_file)
 
-if __name__ == "__main__":
+if __name__ == "__main__": #this is so I can call as an arg in a cmd line 
     parser = argparse.ArgumentParser(description="Process 3D mesh files.")
     parser.add_argument("input_file", help="Input mesh file to process.")
     parser.add_argument("output_file", help="Output path for the processed mesh.")
